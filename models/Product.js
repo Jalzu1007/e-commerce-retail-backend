e6c6259accd5361bedb44b970ae09f90f5276c3b
Jose Alzuri <jalzu1007@gmail.com>
2023-08-04 13:18:31 -0400

    Added models product for backend application

--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,47 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
+{
     // define columns
+  product_name: {
+    type: DataTypes.STRING
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  price: { 
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    isDecimal: true,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    isNumeric: true,
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    // References the category model's id
+    references: {
+      model: "category",
+      key: "id",
+    },
+  },
+},
+
+{
+   // Link to database connection
+  sequelize,
+  // Set to false to remove 'created_at', and 'updated_at'
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+}
 );
 
 module.exports = Product;
