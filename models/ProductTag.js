eebf10004946f8524b6530a66c27492e7fe410eb
Jose Alzuri <jalzu1007@gmail.com>
2023-08-04 15:09:28 -0400

    Added models ProductTag to backend application

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,44 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
+{
+  // define columns
+  // Remember to add product id and tag id
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "product",
+      key: "id",
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "tag",
+      key: "id",
+    },
+  },
+},
+
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product_tag',
+}
 );
 
 module.exports = ProductTag;
